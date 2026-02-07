9ac762c29d382aa5452a2207d24859fcccf5dcd9
Miguel-Machado85 <miguel.machado8506@gmail.com>
2026-02-07 18:52:44 -0500

    (feat) Creacion metodos de edicion de reserva y espacio, borrado de reserva, y get de reservas futuras por espacio, con sus enrutamientos para conexion al front

--- controllers/reservaController.js
+++ controllers/reservaController.js
@@ -3,9 +3,11 @@ const { Usuario, sequelize } = require("../models");
 const { Admin } = require("../models");
 const { Vecino } = require("../models");
 const { Reserva } = require("../models");
+const { Espacio } = require("../models");
 const bcrypt=require("bcryptjs");
 const { where } = require("sequelize");
 const jwt = require("jsonwebtoken");
+const { Op } = require('sequelize');
 
 const createReserva = async(req, res) => {
     try {
@@ -49,14 +51,92 @@ const getReservaById = async (req, res) => {
         const reserva = await Reserva.findOne({where: {id}})
 
         if(!reserva){
-            return res.tatus(404).json({error: "No existe esta reserva"})
+            return res.status(404).json({error: "No existe esta reserva"})
         }
 
         return res.status(200).json(reserva); 
 }
 
+const getReservasByUsuarioId = async (req, res) => {
+    try {
+        const {usuarioId} = req.params
+
+        const hoy = new Date();
+        hoy.setHours(0, 0, 0, 0)
+
+        const reservas = await Reserva.findAll({ where: { 
+            usuarioId, 
+            fecha: {
+                [Op.gte]: hoy
+            }, 
+        }, include: [
+                {
+                    model: Espacio,
+                    as: "espacio",
+                    attributes: ["nombre","descripcion"],
+                },
+            ],
+            order: [['fecha', 'ASC']]
+    });
+
+    if(!reservas){
+        return res.status(404).json({error: "Este usuario no tiene reservas"})
+    }
+
+        return res.status(200).json(reservas)
+    } catch (error) {
+        return res.status(500).json({error: error.message})
+    }
+}
+
+const deleteReserva = async (req,res) => {
+    try {
+        const {id} = req.params
+
+        const reserva = await Reserva.findOne({where: {id}})
+
+        if(!reserva){
+            return res.status(404).json({error: "No existe esta reserva"})
+        }
+
+         await reserva.destroy();
+
+        return res.status(200).json({ message: "Reserva eliminada correctamente" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({error: "error al eliminar la reserva"});
+    }
+}
+
+const updateReserva = async (req,res) => {
+    try {
+        const {id} = req.params
+        const {fecha, horaInicio, horaFin, cantidadPersonas} = req.body;
+
+        const reserva = await Reserva.findByPk(id);
+        if(!reserva){
+            return res.status(404).json({ message: "Reserva no encontrada"});
+        }
+
+        if(fecha) reserva.fecha = fecha
+        if(horaInicio) reserva.horaInicio = horaInicio
+        if(horaFin) reserva.horaFin = horaFin
+        if(cantidadPersonas) reserva.cantidadPersonas = cantidadPersonas
+
+        await reserva.save();
+
+        return res.status(200).json({message: "Reserva actualizada correctamente"})
+    } catch (error) {
+        console.error('Error al actualizar reserva: ', error);
+        return res.status(500).json({error: error.message});
+    }
+}
+
 module.exports = {
     createReserva,
     getReservas,
-    getReservaById
+    getReservaById,
+    getReservasByUsuarioId,
+    deleteReserva,
+    updateReserva
 };
--- routes/reservaRoutes.js
+++ routes/reservaRoutes.js
@@ -4,6 +4,8 @@ const ReservaController = require("../controllers/reservaController");
 
 router.post("/create", ReservaController.createReserva);
 router.get("/get", ReservaController.getReservas);
-router.get("/get/:id", ReservaController.getReservaById)
-
+router.get("/get/:id", ReservaController.getReservaById);
+router.get("/getU/:usuarioId", ReservaController.getReservasByUsuarioId);
+router.delete("/delete/:id", ReservaController.deleteReserva);
+router.put("/update/:id", ReservaController.updateReserva)
 module.exports = router;
